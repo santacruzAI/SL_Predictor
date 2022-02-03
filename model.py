5d43e79652601b8e3633c2d8082c2ee31d6bd3fa
Hannah McDonald <hmmcdona@ucsc.edu>
2022-02-02 18:44:27 -0800

    Update model.py
    Removed SL_Pairs class. Changed to use sklearn train_test_split to split data.

--- model.py
+++ model.py
@@ -6,40 +6,24 @@ import torch
 import pickle
 import torch.nn as nn
 from torch.optim import Adam
-from torch.utils.data import Dataset, random_split
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import train_test_split
 
 file = open("data.p", "rb")
 data_table = pickle.load(file)
 df = pd.read_pickle("data.p")
 file.close()
-print(df)
-#df = pd.DataFrame(data_table, columns = data_table[0])
+
 X = df.loc[:, df.columns.values[0:-1]].to_numpy()
 y = df.loc[:, 'SL'].to_numpy()
 
-print(X)
-class SL_Pairs(Dataset):
-  def __init__(self, X, y):
-    # convert to tensors
-    self.functs = torch.FloatTensor(X)
-    self.labels = torch.FloatTensor(y)
-
-    # Why would we need another dimension? for bias? only need bias for features
-    #self.functs = self.functs.unsqueeze(1) # add extra dimension for torch
-    #self.labels = self.labels.unsqueeze(1)
-
-  def __len__(self):
-    assert len(self.functs) == len(self.labels) # ensure 1-to-1 correspondence
-    return len(self.labels)
-
-  def __getitem__(self, i):
-    return self.functs[i], self.labels[i] # return X, y pair
-
-
-    
-data = SL_Pairs(X, y)
-trainCount = int(0.8 * len(data)) # percent of data for training
-train, test = random_split(data, [trainCount, len(data) - trainCount])
+# Split data into training and test sets 
+x_train, x_test, y_train, y_test = train_test_split(X, y, train_size=0.8, shuffle=True, random_state=0)
+# Convert to tensors
+x_train = torch.FloatTensor(x_train)
+y_train = torch.FloatTensor(y_train)
+x_test = torch.FloatTensor(x_test)
+y_test = torch.FloatTensor(y_test)
 
 epochs = 100
 class Model(nn.Module):
@@ -49,31 +33,18 @@ class Model(nn.Module):
     self.input = nn.Linear(112, 150)  
     self.fc_1 = nn.Linear(150, 100)
     self.output = nn.Linear(100, 1)
-    self.criterion = nn.BCELoss()
-    # self.input = nn.Linear(112, 150)  
-    # self.fc_1 = nn.Linear(150, 300)
-    # self.fc_2 = nn.Linear(300, 450)
-    # self.fc_3 = nn.Linear(450, 300)
-    # self.fc_4 = nn.Linear(300, 150)
-    # self.output = nn.Linear(150, 1)
-    
+    self.criterion = nn.BCELoss()    
   
   def forward(self, x):
     x = self.input(x)
     x = nn.ReLU()(x)
     x = self.fc_1(x)
     x = nn.ReLU()(x)
-    # x = self.fc_2(x)
-    # x = nn.ReLU()(x)
-    # x = self.fc_3(x)
-    # x = nn.ReLU()(x)
-    # x = self.fc_4(x)
-    # x = nn.ReLU()(x)
     x = self.output(x)
     x= nn.Sigmoid()(x)
     return x 
     
-  def fit(self, train_data):
+  def fit(self, x_train, y_train):
     """ 
     Fits the model to the training data.
     """
@@ -83,7 +54,7 @@ class Model(nn.Module):
     accuracy = []
     for epoch in range(epochs):
       acc = 0
-      for (X, y) in train:
+      for X, y in zip(x_train, y_train):
         optimizer.zero_grad()
         out = self.forward(X)
         if (out.item()<0.5 and y.item() == 0) or (out.item()>=0.5 and y.item()==1):
@@ -91,11 +62,14 @@ class Model(nn.Module):
         loss = self.criterion(out, y)
         loss.backward()
         optimizer.step()
-      acc /= len(train)
+      acc /= len(x_train)
       accuracy.append(acc)
       print('Epoch [%d/%d]\tLoss:%.4f\tAcc: %.4f' % (epoch + 1, epochs, loss.item(), acc))
   
   def evaluate(self, test_data):
+    """
+    Evaluates the model on the test set and returns the accuracy
+    """
     self.eval()
     acc = 0
     for (X, y) in test_data:
@@ -104,13 +78,17 @@ class Model(nn.Module):
           acc += 1
       loss = self.criterion(out, y)
     return(acc/len(test_data))
-      
-    
-
+ 
 
 # model initialization
 model = Model()
-model.fit(train)
-  
-# Grid search for hyperparameters
-# K-fold
+print("here")
+model.fit(x_train, y_train)
+# K-fold cross validation
+k = 10
+# Split the data into k number of folds
+kf = StratifiedKFold(n_splits=k, shuffle=True)
+#for train_idk, test_idx in kf(x_train, y_train)
+
+
+#model.fit(train)
