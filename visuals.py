1b9e6421ebc0bbee6a462419ad25e0e02b81726f
Hannah McDonald <hmmcdona@ucsc.edu>
2022-03-29 16:38:11 -0700

    Added seed variable.

--- visuals.py
+++ visuals.py
@@ -11,6 +11,8 @@ import seaborn as sns
 from sklearn.manifold import TSNE
 from model import *
 
+SEED = 77
+
 def tSNE(model):
   """Creates a t-SNE plot for a given trained model"""
   results = np.squeeze(model.train_results, 1)
@@ -18,7 +20,7 @@ def tSNE(model):
   class_pred = np.where(results >= 0.5, np.ones(results.shape), np.zeros(results.shape)) # label predictions based on raw probabilities
 
   # Perform t-SNE dimensionality reduction
-  m = TSNE(init='pca', method='exact', n_iter=1000, learning_rate=200, perplexity=15, random_state=77)
+  m = TSNE(init='pca', method='exact', n_iter=1000, learning_rate=200, perplexity=15, random_state=SEED)
   tsne_features = m.fit_transform(model.x_train)
   tsne_df = pd.DataFrame({'x':tsne_features[:, 0], 'y':tsne_features[:, 1], "SL Prediction":class_pred})
   # Plot t-SNE results
